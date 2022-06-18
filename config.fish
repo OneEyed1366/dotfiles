34585814ad2bbc0be63a4e3d552b1d8543b2ec5b
psevdo:Proger <psevdoproger@yandex.ru>
2022-06-18 13:39:52 +0300

    fix

--- config.fish
+++ config.fish
@@ -3,14 +3,16 @@ if status is-interactive
 end
 
 set -gx nvm_prefix $(brew --prefix nvm)
-bind `1 _fzf_search_directory
-bind `q _fzf_search_git_log
-bind `w _fzf_search_git_status
-bind `2 _fzf_search_history
+
+bind `` _fzf_search_directory
+bind `h _fzf_search_history
+bind `- _fzf_search_git_status
+bind `= _fzf_search_git_log
 bind `s '_fzf_search_variables (set --show | psub) (set --names | psub)'
 bind `a _fzf_search_processes
 
 export PATH="$PATH":"$HOME/.pub-cache/bin"
+
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
 
