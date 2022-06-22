db4f56cda3032563e38908a94a2caf388ddea775
A.Prokopenko <70268975+OneEyed1366@users.noreply.github.com>
2022-06-22 20:54:32 +0300

    Update git-conf.sh

--- git-conf.sh
+++ git-conf.sh
@@ -1,7 +1,9 @@
 echo "Git -> Configuring aliases..."
 git config --global alias.get "clone http://github.com/OneEyed1366/OneEyed1366.git -b"
 git config --global alias.cm "commit -m"
+git config --global alias.acm = "!f() { git add -A && git commit -m \"$1\"; }; f"
 git config --global alias.commend "commit --amend --no-edit"
+git config --global alias.acommend = "!f() { git add -A && git commit --amend --no-edit; }; f"
 git config --global alias.it "init && git commit -m 'root commit' --allow-empty"
 git config --global alias.shorty "status --short --branch"
 git config --global alias.grog \
@@ -10,3 +12,4 @@ git config --global alias.acp '!f() { git add -A && git commit -m "$1" && git pu
 git config --global alias.acpf '!f() { git add -A && git commit -m "$1" && git push --force; }; f'
 git config --global alias.acplease '!f() { git add -A && git commit -m "$1" && git push --force-with-lease; }; f'
 git config --global alias.nbp '!f() { git checkout -b $1 7& git push --set-upstream origin $1; }; f'
+get = pull --rebase
