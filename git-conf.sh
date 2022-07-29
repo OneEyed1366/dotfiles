e7785036a0cae6ee143a21919289beb92e6677ef
A.Prokopenko <70268975+OneEyed1366@users.noreply.github.com>
2022-07-29 09:02:13 +0300

    Update branch aliases

--- git-conf.sh
+++ git-conf.sh
@@ -9,9 +9,6 @@ echo "Git -> Configuring aliases..."
 git config --global alias.get "clone http://github.com/OneEyed1366/OneEyed1366.git -b"
 git config --global alias.cm "commit -m"
 git config --global alias.acm "!f() { git add -A && git commit -m \"$1\"; }; f"
-git config --global alias.delete-branch '!f() { git push -d "$1" "$2" && git branch -d "$2"; }; f'
-git config --global alias.delete-remote-branch '!f() { git push -d "$1" "$2"; }; f'
-git config --global alias.delete-local-branch '!f() { git branc -d "$1"; }; f'
 git config --global alias.commend "commit --amend --no-edit"
 git config --global alias.acommend = "!f() { git add -A && git commit --amend --no-edit; }; f"
 git config --global alias.it "init && git commit -m 'root commit' --allow-empty"
@@ -21,6 +18,11 @@ git config --global alias.grog \
 git config --global alias.acp '!f() { git add -A && git commit -m "$1" && git push; }; f'
 git config --global alias.acpf '!f() { git add -A && git commit -m "$1" && git push --force; }; f'
 git config --global alias.acplease '!f() { git add -A && git commit -m "$1" && git push --force-with-lease; }; f'
-git config --global alias.nbp '!f() { git checkout -b $1 7& git push --set-upstream origin $1; }; f'
+
 git config --global alias.get "pull --rebase"
-git config --global alias.branches "!f() { git branch --all; }; f"
+echo "Git -> Configuring branch aliases"
+git config --global alias.bb "!f() { git branch --all; }; f"
+git config --global alias.bnp '!f() { git checkout -b $1 7& git push --set-upstream origin $1; }; f'
+git config --global alias.bd '!f() { git push -d "$1" "$2" && git branch -d "$2"; }; f'
+git config --global alias.bdr '!f() { git push -d "$1" "$2"; }; f'
+git config --global alias.bdl '!f() { git branch -d "$1"; }; f'
